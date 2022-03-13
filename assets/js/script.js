edcfb7ec51b112e12563f458b7b1bdad5e91c3e2
Amanda Webster <amandaksierras@gmail.com>
2022-03-13 19:06:26 +1100

    attempting bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 const startBtn = document.querySelector(".start-btn button")
 const quizBox = document.querySelector(".quiz-box")
-const optionList = document.querySelector(".option-list");
 
 //when start quiz button is clicked
 startBtn.onclick = () =>{
         startBtn.classList.remove("activeStart") //hide the start btn
         quizBox.classList.add("activeQuiz") //show the quiz box
         showQuestions(0);
+        questionCounter(1);
 }
 var questionCount = 0;
+var questionNumb = 1;
+var userScore = 0;
+var counter;
+var counterLine;
+var widthValue = 0;
 
-const nextBtn = quizBox.querySelector("footer .next-btn");
+const nextBtn = document.querySelector("footer .next-btn");
 
 //if next button clicked
 nextBtn.onClick = () => {
@@ -31,4 +36,10 @@ nextBtn.onClick = () => {
                         + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
                 questionText.innerHTML = questionTag;
                 optionList.innerHTML = optionTag;
-        }
+}
+        
+function questionCounter() {
+        const lowerqCounter = quizBox.querySelector(".total-questions");
+        var totalQuestionCountTag = '<span><p>' + questionCount + '</p>Of<p>' + questions.length + '</p>Questions</span>';
+        lowerqCounter.innerHTML = totalQuestionCountTag;
+}
